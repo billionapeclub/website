5c254ec6c65971cf85596869d1a894626080740d
John Wong <johnasleyw@gmail.com>
2022-02-16 20:43:16 +0800

    update

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import os
 import pymongo
 from pymongo import MongoClient
 from dotenv import load_dotenv
-from flask import Flask, render_template, url_for, request, jsonify
+from flask import Flask, render_template, url_for, request
+from flask_paginate import Pagination, get_page_args
 
 load_dotenv()
 
@@ -12,16 +13,7 @@ billionape = db.billionape
 rarities = db.rarities
 pages = db.pages
 
-def create_app():
-    app = Flask(__name__)
-
-    @app.route('/')
-    def home():
-        return render_template('index.html')
-
-    @app.route('/gallery-1', methods=["POST", "GET"])
-    def collections():
-        results = [
+results = [
             (
                 entry["_id"],
                 entry["name_apes"],
@@ -30,12 +22,9 @@ def create_app():
             )
             for entry in billionape.find({})
         ]
-        numbers = range(len(results))
-        return render_template("/gallery/gallery-1.html", entries=results, numbers=numbers)
+bac_images = range(1826)
 
-    @app.route('/amc')
-    def amccollection1():
-        results = [
+amc_results = [
             (
                 entry["_id"],
                 entry["rarities"],
@@ -45,11 +34,118 @@ def create_app():
                 entry["rarity_score"],
                 entry["image_link"],
                 entry["web_link"],
+                entry['mint_link'],
             )
             for entry in rarities.find({})
         ]
+amc_images = range(len(amc_results))
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def home():
+        return render_template('index.html')
+
+    
+    def get_bac_images(page, offset=0, per_page=10):
+        offset = ((page - 1) * 52)
+        return bac_images[offset: offset + per_page]
+
+    @app.route('/gallery')
+    def collections():
+        page, per_page, offset = get_page_args(page_parameter='page', 
+                                               per_page_parameter='per_page'
+                                               )
+        per_page = 52
+        total = len(bac_images)
+        pagination_users = get_bac_images(page=page,
+                                      offset=offset, 
+                                      per_page=per_page
+                                      )
+
+
+        pagination = Pagination(page=page, 
+                                per_page=per_page, 
+                                total=total, 
+                                css_framework='bootstrap5'
+                                )
+        
         numbers = range(len(results))
-        return render_template('/amc/amc.html', entries=results, numbers=numbers)
+        return render_template("/gallery/gallery.html", 
+                                entries=results, 
+                                numbers=numbers, 
+                                users=pagination_users,
+                                page=page,
+                                per_page=per_page,
+                                pagination=pagination,
+                                )
+
+    def get_amc_images(page, offset=0, per_page=10):
+        offset = ((page - 1) * 52)
+        return amc_images[offset: offset + per_page]
+
+    @app.route('/amc')
+    def amccollection():
+        page, per_page, offset = get_page_args(page_parameter='page', 
+                                               per_page_parameter='per_page'
+                                               )
+        per_page = 52
+        total = len(amc_images)
+        pagination_users = get_amc_images(page=page,
+                                      offset=offset, 
+                                      per_page=per_page
+                                      )
+
+
+        pagination = Pagination(page=page, 
+                                per_page=per_page, 
+                                total=total, 
+                                css_framework='bootstrap5'
+                                )
+        
+        numbers = range(len(amc_results))
+        return render_template("/amc/amc.html", 
+                                entries=amc_results, 
+                                numbers=numbers, 
+                                users=pagination_users,
+                                page=page,
+                                per_page=per_page,
+                                pagination=pagination,
+                                )
+
+    def get_amc_apes(page, offset=0, per_page=10):
+        offset = ((page - 1) * 1)
+        return amc_images[offset: offset + per_page]
+
+    @app.route('/amc/apes')
+    def amc_apes():
+        page, per_page, offset = get_page_args(page_parameter='page', 
+                                               per_page_parameter='per_page'
+                                               )
+        per_page = 1
+        total = len(amc_images)
+        pagination_users = get_amc_apes(page=page,
+                                      offset=offset, 
+                                      per_page=per_page
+                                      )
+
+
+        pagination = Pagination(page=page, 
+                                per_page=per_page, 
+                                total=total, 
+                                css_framework='bootstrap5'
+                                )
+        
+        numbers = range(len(amc_results))
+        return render_template("/amc/apes/ape-1.html", 
+                                entries=amc_results, 
+                                numbers=numbers, 
+                                users=pagination_users,
+                                page=page,
+                                per_page=per_page,
+                                pagination=pagination,
+                                )
 
     if __name__ == '__main__':
         app.run(debug=True)
